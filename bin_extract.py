ff5a2ef33dcd4fc01e5be2373939a7b3df5d461c
artus25200 <68189043+artus25200@users.noreply.github.com>
2022-08-23 21:10:42 +0200

    only need some polish

--- bin_extract.py
+++ bin_extract.py
@@ -21,6 +21,7 @@ if(len(sys.argv) < 2):
 bin_parts = []
 bin_parts_text = []
 config_json = []
+
 if(not os.path.exists(config) or sys.argv[1]=="config"):
     binary_file = input("binary file : ")
     while(True):
@@ -29,27 +30,31 @@ if(not os.path.exists(config) or sys.argv[1]=="config"):
         offset = input("offset : ")
         size = input("size : ")
         bin_parts.append(BinPart(name, offset, size))
-        config_json.append({"name":f"{name}","offset":f"{offset}","size":f"{size}"})
-        print(config_json)
+        bin_parts_text.append({"name":f"{name}","offset":f"{offset}","size":f"{size}"})
     cfg = open(config,"w")
-    text = f"{{\"binary\":\"{binary_file}\",\"parts\":{json.dumps(bin_parts_json)}}}"
+    text = f"{{\"binary\":\"{binary_file}\",\"parts\":{json.dumps(bin_parts_text)}}}"
     cfg.write(text)
     cfg.close()
 
 
 config_json = json.loads(open(config,"r").read())
 print(config_json) #for debug purposes
+
 binary_file = config_json["binary"]
 print(binary_file) #same
+
 bin_parts_text = config_json["parts"]
 print(bin_parts_text) #same
 
+for parts in bin_parts_text:
+    bin_parts.append(BinPart(parts["name"],parts["offset"],parts["size"]))
+
 if(sys.argv[1]=="unpack"):
     f = open(binary_file, "rb")
-    for part in firmware_parts:
+    for part in bin_parts:
         outfile = open(part.name, "wb")
-        f.seek(part.offset, 0)
-        data = f.read(part.size)
+        f.seek(int(part.offset,16), 0)
+        data = f.read(int(part.size,16))
         outfile.write(data)
         outfile.close()
         print(f"Wrote {part.name} - {hex(len(data))} bytes")
